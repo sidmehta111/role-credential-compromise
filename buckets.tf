04201a6a5a367c97c4f003826580829eba6d0c32
sidmehta111 <114196641+sidmehta111@users.noreply.github.com>
2023-07-26 14:33:36 -0700

    Update buckets.tf

--- buckets.tf
+++ buckets.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "credcomp" {
     bucket = var.credcomp-s3-bucket
     force_destroy = true
 }
-resource “aws_s3_bucket_public_access_block” “public_access_block” {
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.credcompromise.id
   block_public_acls       = true
   block_public_policy     = true
