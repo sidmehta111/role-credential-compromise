64dad1b428d72707b43590f83c895318a8b98b07
sidmehta111 <siddharthjmehta@gmail.com>
2023-07-27 17:14:20 -0700

    c

--- variables.tf
+++ variables.tf
@@ -15,17 +15,3 @@ variable "scenario-name" {
 variable "credcomp-s3-bucket" {
   default = "prismacloud-credcomp-bucket"
 }
-/* #Cidr blocks that can access EC2 via ssh
-variable "credcomp-cidr-ssh" {
-  default = [
-    "199.167.52.5/32",
-    "137.83.193.95/32"
-  ]
-}
-#Cidr blocks that can access EC2 via http
-variable "credcomp-cidr-http" {
-  default = [
-    "199.167.52.5/32",
-    "137.83.193.95/32"
-  ]
-} */
